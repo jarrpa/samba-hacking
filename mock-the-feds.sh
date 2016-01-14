36776cf65439674b8376efe5543a25ecaca49efa
Jose A. Rivera <jarrpa@samba.org>
2016-01-14 08:06:19 -0600

    mock script updates.

--- mock-the-feds.sh
+++ mock-the-feds.sh
@@ -2,6 +2,9 @@
 
 SCM="~/projects/samba/samba-perf"
 PKG="~/projects/fedora/samba"
+REPO="~/repo/f23/x86_64"
+REPO_NAME="jarrpa"
+NODES="ganesh"
 
 MOCK_OPTS="--no-clean --without=configure --nocheck --no-cleanup-after"
 if [ "x${1}" == "xclean" ]; then
@@ -18,13 +21,29 @@ VERSION=$(grep "define samba_version" samba.spec | awk '{print $3}')
 RELEASE=$(grep "define main_release" samba.spec | awk '{print $3}')
 SRPM="samba-${VERSION}-${RELEASE}.fc23.src.rpm"
 
+if [ -f "${REPO}/${SRPM}" ]; then
+  OLD_MD5=`md5sum ${REPO}/${SRPM} | awk '{print $1}'`
+else
+  OLD_MD5=""
+fi
+
 pushd $SCM
 git archive --format=tar.gz --prefix=samba-${VERSION}/ HEAD -o $PKG/samba-${VERSION}.tar.gz
 popd
 
 fedpkg --dist f23 srpm
 
-echo "sudo mock ${MOCK_OPTS} -r f23-x86_64 rebuild ${SRPM}"
-sudo mock ${MOCK_OPTS} -r f23-x86_64 rebuild ${SRPM}
-popd
-vagrant rsync
+NEW_MD5=`md5sum ${SRPM} | awk '{print $1}'`
+
+if [ "${NEW_MD5}" != "${OLD_MD5}" ]; then
+  echo "sudo mock ${MOCK_OPTS} -r f23-x86_64 rebuild ${SRPM}"
+  sudo mock ${MOCK_OPTS} -r f23-x86_64 rebuild ${SRPM}
+  popd
+  vagrant rsync
+  if [ "x${OLD_MD5}" != "x" ]; then
+    CMD="SAMBA_PKGS=`dnf list installed | grep \"samba\\\|ctdb\\\|libwb\\\|libsmb\" | awk '{print $1}'`; sudo dnf reinstall ${SAMBA_PKGS}"
+  else
+    CMD="sudo dnf --disablerepo=* --enablerepo=${REPO_NAME} update"
+  fi
+  vagrant ssh ${NODES} -c "${CMD}"
+fi
